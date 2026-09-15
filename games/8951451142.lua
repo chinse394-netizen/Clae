593c912eaa7e0381c8cc163345ddf3120412aefd
chinse394-netizen <chinse394@gmail.com>
2026-09-14 19:59:05 -0700

    Update games/8951451142.lua for Clae loader paths

--- games/8951451142.lua
+++ games/8951451142.lua
@@ -1,9 +1,8 @@
-
 local vape = shared.vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : ' .. err, 30, 'alert')
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
 	end
 	return res
 end
@@ -11,18 +10,18 @@ local isfile = isfile or function(file)
 	local suc, res = pcall(function()
 		return readfile(file)
 	end)
-	return suc and res ~= nil and res ~= '' 
+	return suc and res ~= nil and res ~= ''
 end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/5rmsn4tt2c-ux/levi_shakingrass/'.. readfile('levi_shakingrass/profiles/commit.txt').. '/'.. select(1, path:gsub('levi_shakingrass/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/chinse394-netizen/Clae/'..readfile('Clae/profiles/commit.txt')..'/'..select(1, path:gsub('Clae/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'.. res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
 		writefile(path, res)
 	end
@@ -30,15 +29,15 @@ local function downloadFile(path, func)
 end
 
 vape.Place = 8768229691
-if isfile('levi_shakingrass/games/' .. vape.Place .. '.lua') then
-	loadstring(readfile('levi_shakingrass/games/' .. vape.Place .. '.lua'), tostring(vape.Place))()
+if isfile('Clae/games/'..vape.Place..'.lua') then
+	loadstring(readfile('Clae/games/'..vape.Place..'.lua'), 'skywars')()
 else
 	if not shared.VapeDeveloper then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/5rmsn4tt2c-ux/levi_shakingrass/'.. readfile('levi_shakingrass/profiles/commit.txt').. '/games/'.. vape.Place.. '.lua', true)
+			return game:HttpGet('https://raw.githubusercontent.com/chinse394-netizen/Clae/'..readfile('Clae/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true)
 		end)
 		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('levi_shakingrass/games/' .. vape.Place .. '.lua'), tostring(vape.Place))()
+			loadstring(downloadFile('Clae/games/'..vape.Place..'.lua'), 'skywars')()
 		end
 	end
 end
